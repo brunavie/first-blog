ce3b40ac60f59852cd6f3067f20438c178058397
Bruna Vieira <60660263+brunavie@users.noreply.github.com>
2021-01-04 15:53:33 -0300

    Update Layout.jsx
    blog description update

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -20,11 +20,11 @@ export default ({ children }) => (
                 <aside className="aside">
                     <Sidebar
                         title="A autora"
-                        description="Sou um projeto de programadora, inconformada com o fato de não conseguir cultivar nem uma flor do deserto e que se encontra atualmente em uma ardua batalha contra a procrastinação e as gordurices. Bora programar enquanto o jardim não saí?"
+                        description="Sou uma programadora, inconformada com o fato de não conseguir cultivar nem uma rosa do deserto se quer, e que se encontra atualmente em uma ardua batalha contra a minha falta de talento para jardinagem. Bora programar enquanto o jardim não saí?"
                     />
                     <Sidebar
                         title="Sobre o blog"
-                        description="Uma coletânea da minha louca aventura rumo a terra dos programadores e amantes das plantas. Tá afim de embarcar nessa? Sigam-me marujos!"
+                        description="Uma coletânea da minha louca aventura rumo a terra dos programadores e amantes das plantas."
                     />
                 </aside>
             </Container>
